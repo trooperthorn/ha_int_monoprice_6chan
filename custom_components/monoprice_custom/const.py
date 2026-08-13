5dcbf01bd24f5185516142cc985880b0d8d514a9
trooperthorn <8163700+trooperthorn@users.noreply.github.com>
2026-08-13 07:03:34 -0500

    Modify platforms and add audio service attributes
    Updated the Platinum Platform list to include additional platforms and added service attributes for audio control.

--- custom_components/monoprice_custom/const.py
+++ custom_components/monoprice_custom/const.py
@@ -4,13 +4,14 @@ from homeassistant.const import Platform
 DOMAIN = "monoprice"
 CONF_PORT = "port"
 
-# Platinum Platform list (Enables the new EQ and RS232 features)
+# Platinum Platform list
 PLATFORMS = [
     Platform.MEDIA_PLAYER,
-    Platform.REMOTE,
-    Platform.NUMBER,
     Platform.SWITCH,
     Platform.SENSOR,
+    Platform.NUMBER,
+    Platform.TEXT,
+    Platform.REMOTE,
 ]
 DOMAIN = "monoprice"
 
@@ -22,6 +23,11 @@ CONF_SOURCE_3 = "source_3"
 CONF_SOURCE_4 = "source_4"
 CONF_SOURCE_5 = "source_5"
 CONF_SOURCE_6 = "source_6"
+
+# Service Attributes
+ATTR_BALANCE = "balance"
+ATTR_BASS = "bass"
+ATTR_TREBLE = "treble"
 
 # Compatibility strings for media_player.py imports
 CONF_NAME = "name"
