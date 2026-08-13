4a5c48befc1fc2d1841aa134b25562b1e1c382ac
trooperthorn <8163700+trooperthorn@users.noreply.github.com>
2026-08-12 21:57:35 -0500

    Add wake-up ping and optimize zone status requests

--- custom_components/monoprice_custom/coordinator.py
+++ custom_components/monoprice_custom/coordinator.py
@@ -20,14 +20,47 @@ class MonopriceCoordinator(DataUpdateCoordinator):
     async def _async_update_data(self):
         """Fetch data from the amp via executor job."""
         try:
-            # Wrap synchronous status polling
+            # Wake-up ping
+            try:
+                _LOGGER.debug("Sending wake-up ping to amplifier.")
+                await self.hass.async_add_executor_job(self.api.serial.write, b"\r\n")
+            except Exception as e:
+                _LOGGER.debug("Wake-up ping failed (expected if locked): %s", e)
+
             zones = {}
-            for zone_id in range(11, 17): # Zones 1-6
-                zone_status = await self.hass.async_add_executor_job(
-                    self.api.zone_status, zone_id
-                )
-                if zone_status:
-                    zones[zone_id] = zone_status
+            for i in range(1, 4):
+                for j in range(1, 7):
+                    zone_id = (i * 10) + j
+                    
+                    # Performance optimization: Only build log strings if debug is on
+                    if _LOGGER.isEnabledFor(logging.DEBUG):
+                        _LOGGER.debug("Requesting status for Zone %s", zone_id)
+                        
+                    try:
+                        zone_status = await self.hass.async_add_executor_job(
+                            self.api.zone_status, zone_id
+                        )
+                        
+                        if zone_status:
+                            if _LOGGER.isEnabledFor(logging.DEBUG):
+                                _LOGGER.debug(
+                                    "Zone %s Response: Power=%s, Volume=%s, Source=%s", 
+                                    zone_id, zone_status.power, zone_status.volume, zone_status.source
+                                )
+                            zones[zone_id] = zone_status
+                            
+                    except Exception as loop_err:
+                        if "Connection timed out" in str(loop_err):
+                            if _LOGGER.isEnabledFor(logging.DEBUG):
+                                _LOGGER.debug("Timeout requesting Zone %s (Unit %s may not exist)", zone_id, i)
+                            if i > 1:
+                                pass # Ignore timeouts for secondary units
+                            else:
+                                raise loop_err
+                        else:
+                            raise loop_err
+                            
             return zones
+            
         except Exception as err:
             raise UpdateFailed(f"Error communicating with API: {err}")
